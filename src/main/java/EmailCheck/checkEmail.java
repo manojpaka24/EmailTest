2e892fe28e72c4aa0668229eda8b53f7e6acfbf0
Manu Paka <manupaka24@gmail.com>
2016-11-12 22:24:21 -0500

    First Commit

--- src/main/java/EmailCheck/checkEmail.java
+++ src/main/java/EmailCheck/checkEmail.java
@@ -11,7 +11,6 @@ import org.testng.annotations.Test;
 import ObjectRepo.Elements;
 
 public class checkEmail {
-
 static WebDriver Net;
 static int j=1;
 static String url = "https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/";
